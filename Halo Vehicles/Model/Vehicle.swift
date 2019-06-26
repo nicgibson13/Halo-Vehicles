b8068ea46a70a90b0721ee1f79fae403c747edd0
Nic Gibson <nic@shpangy.com>
2019-06-26 14:44:16 -0600

    Finished Model and created singleton for Model Controller

---
+++ Halo Vehicles/Model/Vehicle.swift
@@ -0,0 +1,15 @@
+//
+//  Vehicle.swift
+//  Halo Vehicles
+//
+//  Created by Nic Gibson on 6/26/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle: Decodable {
+    var name: String
+    var description: String
+    var smallIconImageURL: URL
+}
---
+++ Halo Vehicles/ModelController/VehicleController.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleController.swift
+//  Halo Vehicles
+//
+//  Created by Nic Gibson on 6/26/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import Foundation
+
+class VehicleController {
+    
+    static let sharedInstance = VehicleController()
+    
+    func fetchVehicleWith (searchTerm: String, completion: @escaping (Vehicle?) -> Void) {
+        
+    }
+}
