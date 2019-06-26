95ce680721ad06e9c75bade7123f98ff22a76d78
Nic Gibson <nic@shpangy.com>
2019-06-26 14:34:29 -0600

    Initial Commit

---
+++ Halo Vehicles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Halo Vehicles
+//
+//  Created by Nic Gibson on 6/26/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
